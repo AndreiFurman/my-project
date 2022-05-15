2b92d4a076b0aec19285b8a31fe7489221d08f76
Andrei Furman <andreifurman26@gmail.com>
2022-05-15 16:08:44 +0300

    adaptive finish

--- js/main.js
+++ js/main.js
@@ -4,9 +4,32 @@ $(function(){
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows:false,
-        dots: true
+        responsive: [{
+        dots: true,
+        breakpoint: 1175,
+        settings:{
+                slidesToShow: 2,
+                slidesToScroll: 2,
+
+            }
+        },
+        {
+            breakpoint: 769,
+        settings:{
+                slidesToShow: 1,
+                slidesToScroll: 1,
+
+            }
+            
+
+        }
+        ]
 
 
     });    
+
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__lext--active')
 
 });
